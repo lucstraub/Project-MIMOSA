36df48ce2c1a766d8c10f9d99cb2ceb273aa9afd
lucstraub <136690466+lucstraub@users.noreply.github.com>
2024-04-22 16:10:10 +0200

    quick fix run.py file

--- run.py
+++ run.py
@@ -16,8 +16,8 @@ for scaling_industry in [0.25]:
     params["emissions"]["baseline carbon intensity"] = False
 
     model1 = MIMOSA(params)
-    #model1.solve()
-    model1.solve(use_neos=True, neos_email="l.straub@uu.nl")
+    model1.solve()
+    #model1.solve(use_neos=True, neos_email="l.straub@uu.nl")
     model1.save(f"testrun_sector_feature_industry_scaling_{scaling_industry}_{datetime.today().strftime('%Y-%m-%d')}")
     
 # model1.plot(filename="result")
